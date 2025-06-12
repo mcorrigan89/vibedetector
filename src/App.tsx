ccd108577219ab68bd54fc92a4fac5005e5978df
Michael Corrigan <mcorrigan89@gmail.com>
2025-06-12 15:09:03 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./index.css";
+
+import logo from "./logo.svg";
+import reactLogo from "./react.svg";
+
+export function App() {
+  return (
+    <div className="max-w-7xl mx-auto p-8 text-center relative z-10">
+      <div className="text-7xl font-bold text-pink-700">the vibes are off</div>
+    </div>
+  );
+}
+
+export default App;
